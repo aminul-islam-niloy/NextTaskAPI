b83304b13e0a1964bb4dff5a10ebc1f9de758f90
aminul-islam-niloy <niloyftaminul@gmail.com>
2024-11-05 20:35:35 +0600

    Created Model and data classes

---
+++ NextTaskAPI/Models/Task.cs
@@ -0,0 +1,12 @@
+﻿namespace NextTaskAPI.Models
+{
+    public class Task
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public bool IsCompleted { get; set; } = false;
+        public bool IsMissed { get; set; } = false;
+    }
+}
